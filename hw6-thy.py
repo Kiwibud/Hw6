ac207df6fb75a70e0a7ed5ed0c75bd5ee43577a9
Thy Nguyen ( pronouns as tee: win) <thynguyen2511@gmail.com>
2019-10-03 15:50:30 -0700

    Update hw6-thy.py
    Up to part 5, part 5 not working

--- hw6-thy.py
+++ hw6-thy.py
@@ -7,15 +7,16 @@
 
 class Product:
     serial_number = 1
+    product_id = ''
+    category = 'GN'
 
     def __init__(self, description, list_price):
         self.description = description
         self.list_price = list_price
         self.stock = 0
-        self.category = 'GN'
-        self.id = self.generate_product_id(self.category)
         self.sales = []
         self.reviews = []
+        self.generate_product_id()
 
     def restock(self, quantity):
         self.stock += quantity
@@ -29,25 +30,23 @@ class Product:
             for i in range(self.stock):
                 self.sales.append(sale_price)
             self.restock(-self.stock)
-            # print(f'{self.stock} Stock')
 
         else:
             for i in range(quantity):
                 self.sales.append(sale_price)
             self.restock(-quantity)
-            # print(f'{self.stock} Stock')
 
     def __str__(self):
-        return f'{self.description}\nProduct ID: {self.id}\n' \
+        return f'{self.description}\nProduct ID: {self.product_id}\n' \
                f'List price: ${self.list_price:.2f}\n' \
                f'Available in stock: {self.stock}'
 
     @classmethod
-    def generate_product_id(cls, category):
+    def generate_product_id(cls):
         serial_num = cls.serial_number
         # update serial number - class variable
         cls.serial_number += 1
-        return f'{category}{serial_num:06}'
+        cls.product_id = f'{cls.category}{serial_num:06}'
 
     @property
     def lowest_price(self):
@@ -67,10 +66,42 @@ class Product:
 
 class VideoGame(Product):
     serial_number = 1
+    category = 'VG'
 
     def __init__(self, description, list_price):
-        self.category = 'VG'
         super().__init__(description, list_price)
+
+
+class Book(Product):
+    serial_number = 1
+    category = 'BK'
+
+    def __init__(self, description, author, pages, list_price):
+        self.author = author
+        self.pages = pages
+        super().__init__(description, list_price)
+
+    # DO WE NEED TO CHECK IF THE SAME BOOK???
+    def __lt__(self, other):
+        return self.pages < other.pages
+
+    def __eq__(self, other):
+        return self.pages == other.pages
+
+
+class Bundle(Product):
+    serial_number = 1
+    category = 'BL'
+
+    def __init__(self, product1, product2, *products):
+        description = product1.description + product2.description
+        price = product1.list_price + product2.list_price
+        for each_product in products:
+            description += each_product.description
+            price += each_product.list_price
+        discount = 0.2
+        price *= (1 - discount)
+        super().__init__(description, price)
 
 
 def main():
@@ -127,6 +158,30 @@ def main():
     lego.sell(10, 20)
     print(lego)
     print(lego.lowest_price)
+    print('-----------------------------------')
+    print('       Testing Book class')
+    print('-----------------------------------')
+    book1 = Book("The Quick Python Book", "Naomi Ceder", 472, 39.99)
+    print(book1.author)
+    print(book1.pages)
+    book1.restock(10)
+    book1.sell(3, 30)
+    book1.sell(1, 32)
+    book1.review(5, "Excellent how to guide")
+    print(book1)
+    print(book1.average_rating)
+    print(book1.lowest_price)
+    book2 = Book("Learning Python", "Mark Lutz", 1648, 74.99)
+    book1.restock(20)
+    book1.sell(2, 50)
+    print(book2)
+    print(book1 > book2)
+    print(book1 < book2)
+    print('-----------------------------------')
+    print('       Testing Bundle class')
+    print('-----------------------------------')
+    bundle1 = Bundle(sunglasses, backpack, mario)
+    print(bundle1)
 
 
 if __name__ == "__main__":
